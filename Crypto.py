8bd928cfdad1a628f6d2ffd3ac9d0d9ed0c78b7d
Shubhansh Jain <93034177+ShubhanshJain@users.noreply.github.com>
2022-04-06 16:35:41 -0700

    Add files via upload

---
+++ Crypto.py
@@ -0,0 +1,35 @@
+# Analyze cryptocurrency using python
+
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import seaborn as sns
+import datetime as dt
+
+currency = "USD"
+metric = "Close"
+
+start = dt.datetime(2022, 1, 1)
+end = dt.datetime.now()
+
+crypto = ['BTC','ETH','LTC','XRP','DASH','SC']
+colnames = []
+
+first = True
+
+for ticker in crypto:
+    data = web.DataReader(f'{ticker}-{currency}', "yahoo", start, end)
+    if first:
+        combined = data[[metric]].copy()
+        colnames.append(ticker)
+        combined.columns = colnames
+        first = False
+    else:
+        combined = combined.join(data[metric])
+        colnames.append(ticker)
+        combined.columns = colnames
+        
+combined = combined.pct_change().corr(method="pearson")
+
+sns.heatmap(combined, annot = True, cmap="coolwarm")
+plt.show()
